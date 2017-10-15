d5f5af80a3e3b706f14858569c6b84a8e22d9fcd
WalkWeb <av18775@list.ru>
2017-10-15 17:17:00 +0300

    v1.0

---
+++ level.sql
@@ -0,0 +1,50 @@
+
+
+
+
+
+-- Таблица Users
+
+CREATE TABLE `users` (
+	`id` INT AUTO_INCREMENT PRIMARY KEY,
+	`name` VARCHAR(255),
+	`lvl`INT DEFAULT 1,
+	`exp` INT DEFAULT 0
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+
+-- Таблица Levels
+
+CREATE TABLE `levels` (
+	`id` INT AUTO_INCREMENT PRIMARY KEY,
+	`lvl` INT DEFAULT NULL,
+	`exp_to_lvl` INT DEFAULT NULL,
+	`exp_total` INT DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- Заполняем таблицу User
+
+INSERT INTO `users` (`id`, `name`, `lvl`, `exp`)
+VALUES
+(NULL, 'Vasya', '1', '0');
+
+-- Заполняем таблицу Levels
+
+INSERT INTO `levels` (`id`, `lvl`, `exp_total`, `exp_to_lvl`)
+VALUES
+(NULL, '1', '0', '25'),
+(NULL, '2', '25', '66'),
+(NULL, '3', '91', '117'),
+(NULL, '4', '208', '175'),
+(NULL, '5', '383', '239'),
+(NULL, '6', '622', '309'),
+(NULL, '7', '931', '383'),
+(NULL, '8', '1314', '462'),
+(NULL, '9', '1776', '544'),
+(NULL, '10', '2320', '1000000');
+
+
+-- Удаление таблиц
+
+DROP TABLE `users`;
+DROP TABLE `levels`;
---
+++ model.php
@@ -0,0 +1,130 @@
+<?php
+
+class Model
+{
+    public $DB;
+
+    public function __construct()
+    {
+        $this->DB = mysqli_connect('localhost', 'game', '12345', 'game')
+        or die('Невозможно подключиться к серверу БД. ' . mysql_error());
+        $this->DB->query('SET NAMES utf8');
+    }
+
+    public function getUserInfo($id)
+    {
+        $queryString = 'SELECT users.name, users.lvl, users.exp 
+                        FROM `users` 
+                        WHERE users.id = ?';
+        if ($stmt = $this->DB->prepare($queryString)) {
+            $stmt->bind_param(
+                "i",
+                $id
+            );
+            $stmt->execute();
+            if (!$stmt->error) {
+                $userinfo = [];
+                $stmt->bind_result(
+                    $userinfo['name'],
+                    $userinfo['lvl'],
+                    $userinfo['exp']
+                );
+                $stmt->fetch();
+            }
+            $stmt->close();
+        }
+
+        return $userinfo;
+    }
+
+    public function getExpToLevel($id)
+    {
+        $queryString = 'SELECT `levels`.`exp_to_lvl` 
+                    FROM levels 
+                    JOIN users ON users.lvl = levels.lvl 
+                    WHERE users.id = ?';
+        if ($stmt = $this->DB->prepare($queryString)) {
+            $stmt->bind_param(
+                "i",
+                $id
+            );
+            $stmt->execute();
+            if (!$stmt->error) {
+                $stmt->bind_result($expToLevel);
+                $stmt->fetch();
+            }
+            $stmt->close();
+        }
+
+        return $expToLevel;
+    }
+
+    public function getExpAtLevel($totalexp, $lvl)
+    {
+        $queryString = 'SELECT ? - exp_total FROM levels WHERE lvl = ?';
+        if ($stmt = $this->DB->prepare($queryString)) {
+            $stmt->bind_param(
+                "ii",
+                $totalexp,
+                $lvl
+            );
+            $stmt->execute();
+            if (!$stmt->error){
+                $stmt->bind_result($expAtLevel);
+                $stmt->fetch();
+            }
+            $stmt->close();
+        }
+
+        return $expAtLevel;
+    }
+
+    public function getLevel($totalexp)
+    {
+        $queryString = 'SELECT MAX(t.lvl) FROM (SELECT lvl FROM levels WHERE exp_total - ? <= 0) t';
+        if ($stmt = $this->DB->prepare($queryString)) {
+            $stmt->bind_param(
+                "i",
+                $totalexp
+            );
+            $stmt->execute();
+            if (!$stmt->error){
+                $stmt->bind_result($level);
+                $stmt->fetch();
+            }
+            $stmt->close();
+        }
+
+        return $level;
+    }
+
+    public function setLevel($id, $lvl)
+    {
+        $queryString = 'UPDATE users SET lvl = ? 
+                          WHERE id = ?';
+        if ($stmt = $this->DB->prepare($queryString)) {
+            $stmt->bind_param(
+                "ii",
+                $lvl,
+                $id
+            );
+            $stmt->execute();
+            $stmt->close();
+        }
+    }
+
+    public function editExp($id, $exp)
+    {
+        $queryString = 'UPDATE users SET exp = ? 
+                          WHERE id = ?';
+        if ($stmt = $this->DB->prepare($queryString)) {
+            $stmt->bind_param(
+                "ii",
+                $exp,
+                $id
+            );
+            $stmt->execute();
+            $stmt->close();
+        }
+    }
+}
